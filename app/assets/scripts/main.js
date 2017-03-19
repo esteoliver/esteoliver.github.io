08bbed57890b437795d442ca2cc55f28358fa7f7
Esteban <esteban.jof@gmail.com>
2017-03-19 15:57:34 -0400

    New landing page with bio

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -5,47 +5,5 @@ app.controller('MainCtrl', [
   '$interval',
   function ($scope, $interval) {
 
-    var names = [
-      'Bandido',
-      'Banquito',
-      'Banco',
-      'Banano',
-      'Bandolero',
-      'Pana'
-    ];
-
-    var images = [
-      'caracas',
-      'code'
-    ];
-
-    var current = 0;
-    var currentImage = 0;
-
-    $scope.name = names[current];
-    $scope.bgImage = images[currentImage];
-
-    $scope.toggleProfessional = function () {
-      $scope.showProfessional = !$scope.showProfessional;
-      $scope.showPersonal = false;
-    };
-
-    $scope.togglePersonal = function () {
-      $scope.showPersonal = !$scope.showPersonal;
-      $scope.showProfessional = false;
-    };
-
-    $interval(function () {
-      // $scope.name = next();
-      $scope.bgImage = nextImage();
-    }, 10000);
-
-    function next () {
-      return names[current++ % names.length];
-    }
-
-    function nextImage () {
-      return images[currentImage++ % images.length];
-    }
   }
 ]);
